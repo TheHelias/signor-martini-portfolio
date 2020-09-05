f38b7e5d54c127a5988e2ede7501a69035deb422
TheHelias <akande.tolu@yahoo.com>
2020-09-05 21:42:36 +0100

    feat: remove unneeded hero section

--- src/components/AboutPageTemplate/index.js
+++ src/components/AboutPageTemplate/index.js
@@ -7,26 +7,12 @@ const AboutPageTemplate = ({ title, content, contentComponent }) => {
 
   return (
     <div>
-      <section className='hero is-primary is-bold is-medium'>
-        <div className='hero-body'>
-          <div className='container'>
-            <div className='columns'>
-              <div className='column is-10 is-offset-1'>
-                <div className='section'>
-                  <h1 className='title'>
-                    {title}
-                  </h1>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
       <section className='section section--gradient'>
         <div className='container'>
           <div className='columns'>
             <div className='column is-10 is-offset-1'>
               <div className='section'>
+                <h1 className='title'>{title}</h1>
                 <PageContent className='content' content={content} />
               </div>
             </div>
@@ -40,7 +26,7 @@ const AboutPageTemplate = ({ title, content, contentComponent }) => {
 AboutPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
-  contentComponent: PropTypes.func,
+  contentComponent: PropTypes.func
 }
 
 export default AboutPageTemplate
