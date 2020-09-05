279b2b10ea5dded62d6c4b18c8bb331d2be3d0a1
TheHelias <akande.tolu@yahoo.com>
2020-09-05 21:49:56 +0100

    feat: remove about page

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -5,13 +5,13 @@ import SearchBox from '../SearchBox'
 const NavBar = ({ toggleNavbar, isActive }) => (
   <StaticQuery
     query={graphql`
-            query SearchIndexQuery {
-                siteSearchIndex {
-                    index
-                }
-            }
-        `}
-    render={data => (
+      query SearchIndexQuery {
+        siteSearchIndex {
+          index
+        }
+      }
+    `}
+    render={(data) => (
       <nav className='navbar is-fixed-top' aria-label='main navigation'>
         <div className='navbar-brand'>
           <Link to='/' className='navbar-item'>
@@ -27,26 +27,23 @@ const NavBar = ({ toggleNavbar, isActive }) => (
             <span />
           </button>
         </div>
-        <div className={`navbar-menu ${isActive ? 'is-active' : ''}`} id='navMenu'>
-
+        <div
+          className={`navbar-menu ${isActive ? 'is-active' : ''}`}
+          id='navMenu'
+        >
           <div className='navbar-end'>
             <SearchBox searchIndex={data.siteSearchIndex.index} />
-            <Link className='navbar-item' to='/about'>
-                            About
-            </Link>
             <Link className='navbar-item' to='/pricing'>
-                            Pricing
+              Pricing
             </Link>
             <Link className='navbar-item' to='/blog'>
-                            Blog
+              Blog
             </Link>
             <div className='navbar-item'>
               <div className='field is-grouped'>
                 <p className='control'>
-                  <Link
-                    className='button is-primary is-outlined'
-                    to='/contact'>
-                            Contact Us
+                  <Link className='button is-primary is-outlined' to='/contact'>
+                    Contact Us
                   </Link>
                 </p>
               </div>
