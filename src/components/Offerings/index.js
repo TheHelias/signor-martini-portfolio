f9529d217ef8bab245a3b09ec19ddd78e0271e42
TheHelias <akande.tolu@yahoo.com>
2020-09-09 22:45:28 +0100

    feat: improve portfolio secttion UI

--- src/components/Offerings/index.js
+++ src/components/Offerings/index.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import { FaPlay } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 
 const Offerings = ({ gridItems }) => (
   <div className='columns is-multiline'>
-    {gridItems.map(item => (
-      <div key={item.image} className='column is-6' style={{ borderRadius: '5px' }}>
+    {gridItems.map((item) => (
+      <div
+        key={item.image}
+        className='column is-6 portfolio--item'
+        style={{ borderRadius: '5px' }}
+      >
         <section className='section'>
-          <p className='has-text-centered'>
-            <img alt='' src={item.image} />
-          </p>
+          <div className='portfolio--item__image'>
+            <p className='has-text-centered'>
+              <img alt='' src={item.image} />
+            </p>
+            <div className='portfolio--item__image--overlay' />
+            <FaPlay className='portfolio--item__image--play-icon' />
+          </div>
           <p>{item.text}</p>
         </section>
       </div>
@@ -20,9 +29,9 @@ Offerings.propTypes = {
   gridItems: PropTypes.arrayOf(
     PropTypes.shape({
       image: PropTypes.string,
-      text: PropTypes.string,
+      text: PropTypes.string
     })
-  ),
+  )
 }
 
 export default Offerings
